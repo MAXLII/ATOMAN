16d96bab6583fee55d82afc7557f44272bd91520
Max.Li <58096952+MAXLII@users.noreply.github.com>
2026-06-02 10:58:46 +0800

    feat: 新增 BOOST PLECS 仿真适配

---
+++ plecs/boost/bsp/bsp_adc.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @file    bsp_adc.c
+ * @brief   PLECS boost ADC adapter module.
+ * @details
+ *          This file is part of the BUCK2 project.
+ *
+ *          Module responsibilities:
+ *          - Keep a C translation unit for the PLECS BSP ADC module
+ *          - Leave ADC code conversion in the public macros declared by bsp_adc.h
+ *          - Avoid interrupt registration and cached measurement state in the BSP layer
+ *
+ *          Design notes:
+ *          - C11 compatible
+ *          - No dynamic memory allocation
+ *          - ISR-safe path should be explicitly documented
+ *          - Hardware access should be abstracted through HAL / BSP
+ *
+ * @author  Max.Li
+ * @date    2026-05-24
+ * @version 1.0.0
+ *
+ * Copyright (c) 2026 Max.Li.
+ * All rights reserved.
+ *
+ * This file is licensed under the MIT License.
+ * See the LICENSE file in the project root for full license text.
+ */
+#include "bsp_adc.h"
