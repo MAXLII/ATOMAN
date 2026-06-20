bb1c1c60898385365fafc8cd6f6fcd0b831e680e
Max.Li <58096952+MAXLII@users.noreply.github.com>
2026-06-21 03:25:05 +0800

    chore: 更新版本号至 2.6.6

--- gd32g553c/tools/fw_info/fw_info.h
+++ gd32g553c/tools/fw_info/fw_info.h
@@ -26,7 +26,7 @@ typedef struct
 #define RELEASE_VER 6 // 发布版本 6
 
 // 调试版本 (低8位中的低4位)
-#define DEBUG_VER 5 // 调试版本 5
+#define DEBUG_VER 6 // 调试版本 6
 // 模块主机类型
 #ifdef IS_LLC
 #define HOST_ADDR 0x02
