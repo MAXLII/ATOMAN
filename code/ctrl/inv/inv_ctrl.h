2c7a47cbf9b32dbc9abdfcd0d947421eecb02175
Max.Li <58096952+MAXLII@users.noreply.github.com>
2026-08-01 05:07:51 +0800

    feat: 实现逆变器同步坐标系电压控制

--- code/ctrl/inv/inv_ctrl.h
+++ code/ctrl/inv/inv_ctrl.h
@@ -33,22 +33,53 @@
 #include "hw_params.h"
 #include "my_math.h"
 
-#define INV_CTRL_VOLT_LOOP_FREQ_CUT (300.0f)
-#define INV_CTRL_VOLT_LOOP_PM (60.0f)
+#define INV_CTRL_RATED_POWER_W (6600.0f)
+#define INV_CTRL_NOMINAL_RMS_V (230.0f)
+#define INV_CTRL_NOMINAL_FREQ_HZ (50.0f)
+#define INV_CTRL_FILTER_IND_RES_OHM (0.001f)
+#define INV_CTRL_NOMINAL_LOAD_OHM                                                                                 \
+    ((INV_CTRL_NOMINAL_RMS_V * INV_CTRL_NOMINAL_RMS_V) / INV_CTRL_RATED_POWER_W)
 
-#define INV_CTRL_VOLT_LOOP_D_FREQ_CUT INV_CTRL_VOLT_LOOP_FREQ_CUT
-#define INV_CTRL_VOLT_LOOP_D_W_CUT (2.0f * M_PI * INV_CTRL_VOLT_LOOP_D_FREQ_CUT)
-#define INV_CTRL_VOLT_LOOP_D_PM INV_CTRL_VOLT_LOOP_PM
-#define INV_CTRL_VOLT_LOOP_D_KP (sinf(INV_CTRL_VOLT_LOOP_D_PM / 180.0f * M_PI) * INV_CTRL_VOLT_LOOP_D_W_CUT * HW_AC_SIDE_CAP_VALUE)
-#define INV_CTRL_VOLT_LOOP_D_KI (INV_CTRL_VOLT_LOOP_D_KP * INV_CTRL_VOLT_LOOP_D_W_CUT / tanf(INV_CTRL_VOLT_LOOP_D_PM / 180.0f * M_PI))
+#define INV_CTRL_INNER_LOOP_BANDWIDTH_HZ (4000.0f)
+#define INV_CTRL_INNER_LOOP_BANDWIDTH_RADPS (M_2PI * INV_CTRL_INNER_LOOP_BANDWIDTH_HZ)
+#define INV_CTRL_INNER_LOOP_SQRT_TERM                                                                             \
+    ((2.0f * INV_CTRL_FILTER_IND_RES_OHM * HW_AC_SIDE_CAP_VALUE * INV_CTRL_NOMINAL_LOAD_OHM *                     \
+      ((INV_CTRL_FILTER_IND_RES_OHM * HW_AC_SIDE_CAP_VALUE * INV_CTRL_NOMINAL_LOAD_OHM) +                         \
+       HW_AC_SIDE_IND_VALUE)) +                                                                                    \
+     (HW_AC_SIDE_IND_VALUE * HW_AC_SIDE_IND_VALUE *                                                               \
+      (2.0f +                                                                                                      \
+       (HW_AC_SIDE_CAP_VALUE * HW_AC_SIDE_CAP_VALUE *                                                             \
+        INV_CTRL_NOMINAL_LOAD_OHM * INV_CTRL_NOMINAL_LOAD_OHM *                                                   \
+        INV_CTRL_INNER_LOOP_BANDWIDTH_RADPS * INV_CTRL_INNER_LOOP_BANDWIDTH_RADPS))))
+#define INV_CTRL_INNER_LOOP_K                                                                                      \
+    ((HW_AC_SIDE_IND_VALUE +                                                                                       \
+      (INV_CTRL_FILTER_IND_RES_OHM * HW_AC_SIDE_CAP_VALUE * INV_CTRL_NOMINAL_LOAD_OHM) +                          \
+      sqrtf(INV_CTRL_INNER_LOOP_SQRT_TERM)) /                                                                      \
+     (HW_AC_SIDE_CAP_VALUE * INV_CTRL_NOMINAL_LOAD_OHM))
 
-#define INV_CTRL_VOLT_LOOP_Q_FREQ_CUT INV_CTRL_VOLT_LOOP_FREQ_CUT
-#define INV_CTRL_VOLT_LOOP_Q_W_CUT (2.0f * M_PI * INV_CTRL_VOLT_LOOP_Q_FREQ_CUT)
-#define INV_CTRL_VOLT_LOOP_Q_PM INV_CTRL_VOLT_LOOP_PM
-#define INV_CTRL_VOLT_LOOP_Q_KP (sinf(INV_CTRL_VOLT_LOOP_Q_PM / 180.0f * M_PI) * INV_CTRL_VOLT_LOOP_Q_W_CUT * HW_AC_SIDE_CAP_VALUE)
-#define INV_CTRL_VOLT_LOOP_Q_KI (INV_CTRL_VOLT_LOOP_Q_KP * INV_CTRL_VOLT_LOOP_Q_W_CUT / tanf(INV_CTRL_VOLT_LOOP_Q_PM / 180.0f * M_PI))
+#define INV_CTRL_VOLT_LOOP_BANDWIDTH_HZ (1300.0f)
+#define INV_CTRL_VOLT_LOOP_BANDWIDTH_RADPS (M_2PI * INV_CTRL_VOLT_LOOP_BANDWIDTH_HZ)
+#define INV_CTRL_VOLT_LOOP_KP                                                                                      \
+    (HW_AC_SIDE_CAP_VALUE * INV_CTRL_VOLT_LOOP_BANDWIDTH_RADPS *                                                   \
+     (sqrtf((2.0f * HW_AC_SIDE_IND_VALUE * HW_AC_SIDE_IND_VALUE *                                                 \
+             INV_CTRL_VOLT_LOOP_BANDWIDTH_RADPS * INV_CTRL_VOLT_LOOP_BANDWIDTH_RADPS) +                           \
+            (INV_CTRL_INNER_LOOP_K * INV_CTRL_INNER_LOOP_K)) -                                                     \
+      (HW_AC_SIDE_IND_VALUE * INV_CTRL_VOLT_LOOP_BANDWIDTH_RADPS)) /                                               \
+     INV_CTRL_INNER_LOOP_K)
+#define INV_CTRL_VOLT_LOOP_KI_RATIO (0.55f)
+#define INV_CTRL_VOLT_LOOP_KI                                                                                      \
+    (INV_CTRL_VOLT_LOOP_KI_RATIO * INV_CTRL_VOLT_LOOP_KP * M_2PI * INV_CTRL_NOMINAL_FREQ_HZ)
+#define INV_CTRL_VOLT_LOOP_OUT_MAX_A (20.0f)
+#define INV_CTRL_VOLT_LOOP_OUT_MIN_A (-20.0f)
 
-#define INV_CTRL_CURR_LOOP_KR (2.0f)
+#define INV_CTRL_HARMONIC_3_ORDER (3.0f)
+#define INV_CTRL_HARMONIC_5_ORDER (5.0f)
+#define INV_CTRL_HARMONIC_7_ORDER (7.0f)
+#define INV_CTRL_HARMONIC_3_GAIN (0.30f)
+#define INV_CTRL_HARMONIC_5_GAIN (0.20f)
+#define INV_CTRL_HARMONIC_7_GAIN (0.15f)
+#define INV_CTRL_HARMONIC_OUT_MAX_A (8.0f)
+#define INV_CTRL_HARMONIC_OUT_MIN_A (-8.0f)
 
 void inv_ctrl_set_p_hal(inv_ctrl_hal_t *p);
 void inv_ctrl_prepare_run(void);
