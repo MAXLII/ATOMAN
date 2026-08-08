37ab02bada408fbf878dce66a460d9a5be86921e
Max.Li <58096952+MAXLII@users.noreply.github.com>
2026-08-09 00:22:39 +0800

    chore: 更新版本号至 1.2.32.0

--- platform/gd32g553c/tools/fw_info/fw_info.h
+++ platform/gd32g553c/tools/fw_info/fw_info.h
@@ -23,7 +23,7 @@ typedef struct
 #define DEVICE_VENDOR 2 // 设备商代码
 
 // 发布版本 (低8位中的高4位)
-#define RELEASE_VER 31 // release version 31
+#define RELEASE_VER 32 // release version 32
 
 // 调试版本 (低8位中的低4位)
 #define DEBUG_VER 0 // debug version 0
