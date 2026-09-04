6728993ba781379e6f3990fb48e5f3bba22ff1be
Max.Li <58096952+MAXLII@users.noreply.github.com>
2026-09-05 03:09:02 +0800

    refactor: 删除未使用的旧业务与重复工具实现

--- code/app/device_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEVICE_INFO
-#define __DEVICE_INFO
-
-#define PC_ADDR 0x01
-#define LLC_ADDR 0x02
-#define PFC_ADDR 0x03
-#define APP_ADDR 0x04
-
-#endif
--- code/app/pfc/pwr_cmd.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PWR_CMD_H
-#define __PWR_CMD_H
-
-#include "stdint.h"
-
-uint8_t pwr_cmd_allow_dsg(void);
-
-#endif
