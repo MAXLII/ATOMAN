61ffa28ea6b0fb0249e93454215e0e15931b1b13
Max.Li <58096952+MAXLII@users.noreply.github.com>
2026-09-05 03:07:45 +0800

    feat: 为 GD32 BSP 增加定时器抖动观测接口

--- platform/gd32g553c/bsp/inc/bsp_timer.h
+++ platform/gd32g553c/bsp/inc/bsp_timer.h
@@ -4,5 +4,8 @@
 #include <stdint.h>
 
 void bsp_timer_init(void);
+uint32_t bsp_timer_jitter_count_get(void);
+uint32_t bsp_timer_jitter_clock_hz_get(void);
+uint32_t bsp_timer_jitter_period_ticks_get(void);
 
 #endif
--- platform/gd32g553c/bsp/src/bsp_timer.c
+++ platform/gd32g553c/bsp/src/bsp_timer.c
@@ -40,4 +40,19 @@ void bsp_timer_init(void)
     timer_enable(TIMER1);
 }
 
+uint32_t bsp_timer_jitter_count_get(void)
+{
+    return TIMER_CNT(TIMER2) & 0x0000FFFFu;
+}
+
+uint32_t bsp_timer_jitter_clock_hz_get(void)
+{
+    return SystemCoreClock;
+}
+
+uint32_t bsp_timer_jitter_period_ticks_get(void)
+{
+    return (TIMER_CAR(TIMER2) & 0x0000FFFFu) + 1u;
+}
+
 REG_INIT(0, bsp_timer_init)
